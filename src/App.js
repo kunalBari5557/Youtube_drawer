5b067ecab0afd272295ae080ae60e0a39a77a869
kunal <kunalaage21@gmail.com>
2023-10-27 10:12:02 +0530

    hotfix: routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
 import Sidenav from "./Sidenav";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Analytics from "./components/Analytics";
-import Shorts from "./components/Shorts";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Sidenav />
-
-            {/* <Route path="/Home" element={<Home />} />
-            <Route path="/Analytics" element={<Analytics />} />
-            <Route path="/Shorts" element={<Shorts />} /> */}
       </BrowserRouter>
     </div>
   );
